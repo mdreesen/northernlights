312760b86031df76a47356dcbfbfd77f157ed4d3
Michael Dreesen <michaeldreesen90@gmail.com>
2021-08-07 00:52:51 -0400

    adding react parallax to see if it will fit into this project

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Parallax from 'react-rellax'
 
 const Home = () => {
     return (
         <div>
             <div className="homeImgContainer">
-                <h1 className="headerOneText">Northern Lights</h1>
-                <img className="topImg"></img>
+            <h1 className="headerOneText">Northern Lights</h1>
+                <Parallax speed={3}>
+                    <img className="topImg"></img>
+                </Parallax>
             </div>
         </div>
     );
