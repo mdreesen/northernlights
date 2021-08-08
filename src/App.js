44b383093491c9c5b9554b704f9041ff45b478dc
Michael Dreesen <michaeldreesen90@gmail.com>
2021-08-08 15:10:33 -0400

    added in lazy loading for navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-// Components
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
+// import Navigation from './components/Navigation';
+// import Footer from './components/Footer';
 
 // Pages
 import Home from './pages/Home';
 import Interior from './pages/Interior';
+
+// Components, lazy loading
+const Navigation = lazy(() => import('./components/Navigation'))
+const Footer = lazy(() => import('./components/Footer'))
 
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navigation />
+        <Suspense fallback={<div />}>
+          <Navigation />
+        </Suspense>
         <div>
           <Route exact path="/" component={Home} />
           <Route exact path="/northern-lights-interior" component={Interior} />
         </div>
         <div className="footer_section">
-          <Footer/>
+          <Suspense fallback={<div />}>
+            <Footer />
+          </Suspense>
         </div>
       </div>
     </Router>
