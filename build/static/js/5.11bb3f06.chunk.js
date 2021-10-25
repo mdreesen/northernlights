86f56af2ce028293c4d8f3b18b2ab58c0bbacbb4
Michael Dreesen <michaeldreesen90@gmail.com>
2021-10-24 20:16:16 -0400

    added utils folder along with prod build

--- build/static/js/5.11bb3f06.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpnorthernlights=this.webpackJsonpnorthernlights||[]).push([[5],{53:function(n,h,s){}}]);
-//# sourceMappingURL=5.11bb3f06.chunk.js.map
---
+++ build/static/js/7.c57ce8d2.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpnorthernlights=this.webpackJsonpnorthernlights||[]).push([[7],{55:function(n,h,s){}}]);
+//# sourceMappingURL=7.c57ce8d2.chunk.js.map
